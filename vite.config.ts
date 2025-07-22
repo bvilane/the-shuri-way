2a0da87dec940091f8354c28c346b6222df7cb2f
Keshav Garg <keshav2003garg@gmail.com>
2025-07-22 15:27:22 +0530

    feat: sst added

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: { port: 3000 },
   plugins: [
     tsConfigPaths(),
-    tanstackStart({ customViteReactPlugin: true }),
+    tanstackStart({ customViteReactPlugin: true, target: 'aws-lambda' }),
     viteReact(),
     tailwindcss(),
   ],
